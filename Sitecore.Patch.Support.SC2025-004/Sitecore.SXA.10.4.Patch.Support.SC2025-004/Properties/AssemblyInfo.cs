4abca047c3774491ce63fe03512602266efb4917
navan <contactnavaneeth@gmail.com>
2025-07-22 09:55:07 +0000

    SXA 10.4 cumulative hotfix

--- Sitecore.Patch.Support.SC2025-004/Sitecore.SXA.10.4.Patch.Support.SC2025-004/Properties/AssemblyInfo.cs
+++ Sitecore.Patch.Support.SC2025-004/Sitecore.SXA.10.4.Patch.Support.SC2025-004/Properties/AssemblyInfo.cs
@@ -2,32 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.SXA10.4.Patch.Support.SC2025-004")]
+[assembly: AssemblyTitle("Sitecore.SXA._10._4.Patch.Support.SC2025_004")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitecore.SXA10.4.Patch.Support.SC2025-004")]
+[assembly: AssemblyProduct("Sitecore.SXA._10._4.Patch.Support.SC2025_004")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a9cb11cf-1478-448d-bf92-23d67c86b9f9")]
+[assembly: Guid("c9025fb8-2379-45af-b096-d0d5c159ff62")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sitecore.Patch.Support.SC2025-004/Sitecore.SXA.10.4.Patch.Support.SC2025-004/sitecore modules/Shell/PowerShell/UploadFile/PowerShellUploadFile2.aspx
@@ -0,0 +1,2 @@
+<%@ Page language="c#" AutoEventWireup="false" Inherits="Sitecore.XA.Foundation.SPE.UploadFile.PowerShellUploadFilePage2" %>
+<%@ OutputCache Location="None" VaryByParam="none" %>
